9c0dc0745ce73320c5ee55eb76b3280672ae1a4a
Anti-Shulk <justinschwerdtfeger245@gmail.com>
2022-02-16 16:03:39 -0600

    multithrwading auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueCarousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/BlueCarousel.java
@@ -11,6 +11,7 @@ import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.CarouselSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.HardwareSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.MecanumDriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.TurretSubsystem;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 @Autonomous
 @Config
@@ -23,9 +24,20 @@ public class BlueCarousel extends LinearOpMode {
         MecanumDriveSubsystem drive = new MecanumDriveSubsystem(this);
         HardwareSubsystem hardware = new HardwareSubsystem(this);
         ArmSubsystem arm = new ArmSubsystem();
+        TurretSubsystem turret = new TurretSubsystem();
         CarouselSubsystem carousel = new CarouselSubsystem();
 
         Pose2d startPose = new Pose2d(-40.3, 63.2, Math.toRadians(-90));
+
+        Thread outtakeThread = new Thread(() -> {
+            arm.turnAutoPower();
+            arm.moveAutoLow();
+            arm.moveToTarget();
+            while (!arm.wontHitSides()) {
+                sleep(50);
+            }
+            turret.moveToTargetDegrees();
+        });
 
 
 
@@ -40,10 +52,7 @@ public class BlueCarousel extends LinearOpMode {
                 .lineToLinearHeading(new Pose2d(-57, 60.5, Math.toRadians(-145)))
                 .waitSeconds(3)
                 .lineToLinearHeading(new Pose2d(-50, 20, Math.toRadians(-180)))
-
-                .addDisplacementMarker(arm::turnAutoPower)
-                .addDisplacementMarker(arm::moveAutoLow)
-                .addDisplacementMarker(arm::moveToTarget)
+                .addDisplacementMarker(outtakeThread::run)
                 .lineToLinearHeading(new Pose2d(selected, Math.toRadians(-180)))
                 .addDisplacementMarker(20, carousel::stop)
                 .addDisplacementMarker(20, carousel::drop)
